d6fa882a1c063b1ee61282448b256dfae355638b
lowrybg <lowrybg@gmail.com>
2017-10-14 20:28:17 +0300

    Seller and coutry classes added

---
+++ TheAmazingBookStore.Models/Seller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheAmazingBookStore.Models
+{
+   public class Seller
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Rating { get; set; }
+
+        public ICollection<Country> Countries { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
