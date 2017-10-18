8126973c63ff58e3739094a1e2e3a5f3040548ad
lowrybg <lowrybg@gmail.com>
2017-10-18 17:44:11 +0300

    find book class non functional

---
+++ TheAmazingBookStore/TheAmazingBookStore.Controller/Commands/FindCommand/FindBookCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheAmazingBookStore.Controller.Commands.Contracts;
+
+namespace TheAmazingBookStore.Controller.Commands.FindCommand
+{
+    class FindBookCommand : ICommand
+    {
+        private readonly 
+
+        public FindBookCommand()
+        {
+
+        }
+
+        public string Execute(IList<string> parameters)
+        {
+           
+        }
+    }
+}
