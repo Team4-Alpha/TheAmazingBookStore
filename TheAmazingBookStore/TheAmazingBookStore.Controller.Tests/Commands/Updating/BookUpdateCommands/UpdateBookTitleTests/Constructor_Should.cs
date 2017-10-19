2c90658858729e1db559e1882f731f67f599bfef
DESKTOP-CFBG69B\IliyaDamyanov <i.s.damyanov@gmail.com>
2017-10-19 20:52:03 +0300

    Create Delete and Update Constructors Unit Tests

---
+++ TheAmazingBookStore/TheAmazingBookStore.Controller.Tests/Commands/Updating/BookUpdateCommands/UpdateBookTitleTests/Constructor_Should.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity;
+using Moq;
+using TheAmazingBookStore.Models;
+using TheAmazingBookStore.Data.Abstractions;
+using TheAmazingBookStore.Controller.Commands.Updating.BookUpdateCommands;
+
+namespace TheAmazingBookStore.Controller.Tests.Commands.Updating.BookUpdateCommands.UpdateBookTitleTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void CreateInstance_WhenParametersAreCorrect()
+        {
+            // Arrange
+            var mockBook = new Mock<DbSet<Book>>();
+
+            var mockContext = new Mock<IBookStoreContext>();
+            mockContext.Setup(m => m.Books).Returns(mockBook.Object);
+
+            // Act
+            var command = new UpdateBookTitle(mockContext.Object);
+
+            // Assert
+            Assert.IsNotNull(command);
+        }
+
+        [TestMethod]
+        public void ThrowException_WhenContextIsNull()
+        {
+            // Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new UpdateBookTitle(null));
+        }
+    }
+}
